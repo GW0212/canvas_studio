31e1415911e91b5b14a5d0bd70d44cf002eca3cf
Geun Woo Kim <rmsdn1955@gmail.com>
2025-10-22 14:35:20 +0900

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+
+window.FIREBASE_CONFIG = {
+  apiKey: "AIzaSyAIUqtkHgB0vzjU3ndMaNQcIiQ-R_TB6H0",
+  authDomain: "canvas-studio-2f8cb.firebaseapp.com",
+  databaseURL: "https://canvas-studio-2f8cb-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "canvas-studio-2f8cb",
+  storageBucket: "canvas-studio-2f8cb.firebasestorage.app",
+  messagingSenderId: "36647475629",
+  appId: "1:36647475629:web:94e1f18e34d0195945e924",
+  measurementId: "G-5KE41BKVKY"
+};
+console.log('[CanvasStudio] Firebase config loaded', window.FIREBASE_CONFIG.projectId);
